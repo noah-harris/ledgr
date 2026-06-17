faa33ce55a3463ae667babb887b617bc5fd0061e
noah-harris <noahharriscontact@gmail.com>
2026-06-17 08:20:17 -0400

    update

--- client/db.py
+++ client/db.py
@@ -5,10 +5,11 @@ from contextlib import contextmanager
 USERNAME = os.getenv("DB_USERNAME")
 PASSWORD = os.getenv("DB_PASSWORD")
 EXTERNAL_PORT = os.getenv("EXTERNAL_PORT")
+EXTERNAL_HOST= os.getenv("EXTERNAL_HOST")
 
 @contextmanager
 def get_connection(database:str):
-    cxnstr = f"mssql+pyodbc://{USERNAME}:{PASSWORD}@127.0.0.1,{EXTERNAL_PORT}/{database}?driver=ODBC+Driver+17+for+SQL+Server&TrustServerCertificate=yes"
+    cxnstr = f"mssql+pyodbc://{USERNAME}:{PASSWORD}@{EXTERNAL_HOST},{EXTERNAL_PORT}/{database}?driver=ODBC+Driver+17+for+SQL+Server&TrustServerCertificate=yes"
     engine = create_engine(cxnstr, pool_pre_ping=True, connect_args={"timeout": 2})
     conn = engine.connect()
     try:
